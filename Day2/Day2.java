456f53047298c1122cfb0316056355174e4a90ab
cistercii <powerlamp@mail.ru>
2022-04-07 09:46:13 +0300

    Добавил решения 2 дня

--- Day2/Day2.java
+++ Day2/Day2.java
@@ -1,4 +1,124 @@
 package Day2;
 
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Day2 {
+
+    static public void  solution_1() {
+        System.out.println("Высота здания:");
+        Scanner scan = new Scanner(System.in);
+        if (!scan.hasNextInt()) {
+            System.out.println("Ошибка ввода");
+            return;
+        }
+        int h = scan.nextInt();
+        if (h < 1) {
+            System.out.println("Ошибка ввода");
+        } else {
+            if (h < 5) {
+                System.out.println("Малоэтажный дом");
+            } else {
+                if (h < 9) {
+                    System.out.println("Среднеэтажный дом");
+                } else {
+                    System.out.println("Многоэтажный дом");
+                }
+            }
+        }
+    }
+
+    static public void solution_2() throws IOException {
+        int a = 0, b = 0;
+        Scanner scan = new Scanner(System.in);
+        boolean flg = false;
+
+        do { // Проверяем поток ввода до получения числа
+            System.out.println("Введите правильное начало интервала");
+            flg = scan.hasNextInt();
+            if (!flg) {
+                String tmp = scan.next();
+            }
+        } while (!flg);
+        a = scan.nextInt();
+
+        do {
+            System.out.println("Введите правильный конец интервала");
+            flg = scan.hasNextInt();
+            if (!flg) {
+                String tmp = scan.next();
+            }
+        } while (!flg);
+        b = scan.nextInt();
+
+        if (a >= b) {
+            System.out.print("Ошибка ввода");
+            return;
+        }
+
+        for (int i = a + 1; i < b; i++) {
+            if(i % 5 == 0 && i % 10 != 0) {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+        }
+    }
+
+    static public void solution_3() throws IOException {
+        int a = 0, b = 0;
+        Scanner scan = new Scanner(System.in);
+        boolean flg = false;
+
+        do { // Проверяем поток ввода до получения числа
+            System.out.println("Введите правильное начало интервала");
+            flg = scan.hasNextInt();
+            if (!flg) {
+                scan.next();
+            }
+        } while (!flg);
+        a = scan.nextInt();
+
+        do {
+            System.out.println("Введите правильный конец интервала");
+            flg = scan.hasNextInt();
+            if (!flg) {
+                String tmp = scan.next();
+            }
+        } while (!flg);
+        b = scan.nextInt();
+
+        if (a >= b) {
+            System.out.print("Ошибка ввода");
+            return;
+        }
+        int it = a;
+        while  (it < b) {
+            if (it % 5 == 0 && it % 10 != 0) {
+                System.out.print(it + " ");
+            }
+            it++;
+        }
+    }
+
+    static public void solution_4() {
+        Scanner scan = new Scanner(System.in);
+        boolean flg = false;
+        do { // Проверяем поток ввода до получения числа
+            System.out.println("Введите X:");
+            flg = scan.hasNextDouble();
+            if (!flg) {
+                scan.next();
+            }
+        } while (!flg);
+        double X = scan.nextDouble();
+        System.out.println("Введенное значение Х: " + X);
+        double Y = 0.0;
+        if (X >= 5.0) {
+            Y = (Math.pow(X, 2) - 10) / (X + 7);
+        } else if (X > -3.0 && X < 5.0) {
+            Y = (X + 3) * (Math.pow(X, 2) - 2);
+        } else Y = 420.0;
+        System.out.println("Полученное значение Y: " + Y);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
+import Day2.Day2;
+
+import java.io.IOException;
+
 public class Main {
-    public static void main (String [] args) {
-        System.out.println("Done.");
+    public static void main (String [] args) throws IOException {
+
     }
 }
