f10985b42eb437d8790d8c5d72c0b34042a110ba
cistercii <powerlamp@mail.ru>
2022-04-29 09:22:27 +0300

    Добавил решение 16 дня

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import Day14.Day14;
 import Day15.Day15;
+import Day16.Day16;
 
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main (String [] args) throws FileNotFoundException {
-        Day15.solution();
+        Day16.solution_2();
     }
 }
