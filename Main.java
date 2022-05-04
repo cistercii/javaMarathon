0cbe32b7ea8441cd01170bfb371ba0c2a893a751
cistercii <powerlamp@mail.ru>
2022-05-04 08:54:52 +0300

    Добавил решения 18 дня

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import Day17.Day17;
+import Day18.Day18;
 
 import java.io.FileNotFoundException;
 
 public class Main {
     public static void main (String [] args) throws FileNotFoundException {
-        Day17.solution_2();
+        Day18.solution_2();
     }
 }
