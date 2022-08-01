5621ef941a0111c8f64decbb011ea2d2d4dd3a9b
cistercii <powerlamp@mail.ru>
2022-08-01 08:34:52 +0300

    Добавил тесты

---
+++ src/test/java/FinalProject/Players/HumanPlayerTest.java
@@ -0,0 +1,60 @@
+package FinalProject.Players;
+
+import FinalProject.Exceptions.BadInputDataException;
+import FinalProject.FieldPlaying;
+import FinalProject.Game;
+import FinalProject.Ship.SizeDecks;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+public class HumanPlayerTest {
+
+    static Player testPlayer = new HumanPlayer();
+
+    @BeforeClass
+    public static void createFieldPlaying() throws BadInputDataException {
+        FieldPlaying field = testPlayer.getField();
+        field.addShip(SizeDecks.FOUR_DECKS, "А,1;А,2;А,3;А,4");
+        field.addShip(SizeDecks.TWO_DECKS, "В,1;В,2");
+    }
+
+    @Test
+    public void testMiss() throws Exception {
+        String string = "А,6";
+        InputStream is = new ByteArrayInputStream(string.getBytes());
+        System.setIn(is);
+
+        Game.StatusGame status = testPlayer.oneShot(testPlayer);
+        Assert.assertEquals(Game.StatusGame.NEXT_PLAYER_MOVE, status);
+    }
+
+    @Test
+    public void testHit() throws BadInputDataException {
+        String string = "А,3";
+
+        InputStream is = new ByteArrayInputStream(string.getBytes());
+        System.setIn(is);
+
+        Game.StatusGame status = testPlayer.oneShot(testPlayer);
+        Assert.assertEquals(Game.StatusGame.CURRENT_PLAYER_MOVE, status);
+    }
+
+    @Test
+    public void testDestructionShip() throws BadInputDataException {
+        String string = "В,1";
+        InputStream is = new ByteArrayInputStream(string.getBytes());
+        System.setIn(is);
+        testPlayer.oneShot(testPlayer);
+
+        string = "В,2";
+        is = new ByteArrayInputStream(string.getBytes());
+        System.setIn(is);
+
+        Game.StatusGame status = testPlayer.oneShot(testPlayer);
+        Assert.assertEquals(Game.StatusGame.CURRENT_PLAYER_MOVE, status);
+    }
+}
